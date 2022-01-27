6bbd9c57af4a210c3536cd02d79fd98c5c4bca4f
Jérémy Gendre <jgendre@media-sante.com>
2022-01-27 09:03:02 +0100

    remove real datas

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,13 +12,14 @@ import {
     setDoc,
 } from "firebase/firestore";
 
+// TODO: change this
 const firebaseConfig = {
-    apiKey: "AIzaSyCykmw8f9NsDIhq9g07XwFaduQLlei8Pzg",
-    authDomain: "shopping-list-f19c0.firebaseapp.com",
-    projectId: "shopping-list-f19c0",
-    storageBucket: "shopping-list-f19c0.appspot.com",
-    messagingSenderId: "531784564856",
-    appId: "1:531784564856:web:8de1989e49037e457b2d8d"
+    apiKey: "zzzzzzzzzzzzzzzzzzzz",
+    authDomain: "app-name.firebaseapp.com",
+    projectId: "app-name",
+    storageBucket: "app-name.appspot.com",
+    messagingSenderId: "messagingsenderid",
+    appId: "appid"
 };
 
 const app = initializeApp(firebaseConfig);
